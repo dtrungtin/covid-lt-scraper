370d476d89d3867ac1de22d784a74c32f4bb5b2b
dtrungtin <dtrungtin@gmail.com>
2020-03-25 09:42:28 +0700

    Updating

--- main.js
+++ main.js
@@ -49,9 +49,11 @@ Apify.main(async () => {
             }
 
             const [confirmed] = $('.text > ul > li:nth-child(1)').text().match(/\d+/);
-            const [tested] = $('.text > ul > li:nth-child(3)').text().match(/\d+/);
+            const [died] = $('.text > ul > li:nth-child(2)').text().match(/\d+/);
+            const [tested] = $('.text > ul > li:nth-child(5)').text().match(/\d+/);
             data.confirmedCases = parseInt(confirmed);
             data.testedCases = parseInt(tested);
+            data.numberOfDeaths = parseInt(died);
 
             // Compare and save to history
             const latest = await kvStore.getValue(LATEST) || {};
